6fb515cf5bda634b70150291495f00cf380cf4a4
mksmukesh <43673600+mksmukesh@users.noreply.github.com>
2024-02-01 13:15:14 +0545

    Home page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer.','Web Developer.', 'Software Developer.'],
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay:1000,
+    loop:true,
+  });
